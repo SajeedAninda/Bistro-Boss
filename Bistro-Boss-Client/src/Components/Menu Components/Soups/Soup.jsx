a06280cfb1946f9f48ad759099db563c437dcb28
SajeedAninda <sajaninda@gmail.com>
2023-11-11 21:03:48 +0600

    Created Soup Section

---
+++ Bistro-Boss-Client/src/Components/Menu Components/Soups/Soup.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import SoupBanner from './SoupBanner';
+import ListedMenu from '../../Shared/ListedMenu';
+import UseMenu from '../../../Hooks/UseMenu';
+
+const Soup = () => {
+    let menu = UseMenu();
+    let soup = menu.filter(item => item.category === "soup").slice(0, 6);
+    return (
+        <div>
+            <SoupBanner></SoupBanner>
+            <div className='py-12'>
+                <div className='grid grid-cols-2 gap-6 gap-y-10 w-[70%] mx-auto'>
+                    {
+                        soup.map(item => <ListedMenu item={item}></ListedMenu>)
+                    }
+                </div>
+                <div className='flex justify-center mt-8'>
+                    <button className='bg-white px-4 py-2 text-[#1F2937] border-b-4 border-[#1F2937] rounded-xl hover:bg-[#1F2937] hover:text-white'>
+                        Order Your Favourite Food
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Soup;
--- Bistro-Boss-Client/src/Pages/Menu.jsx
+++ Bistro-Boss-Client/src/Pages/Menu.jsx
@@ -4,6 +4,7 @@ import TodaysOffer from '../Components/Menu Components/Todays Offer/TodaysOffer'
 import Deserts from '../Components/Menu Components/Deserts/Deserts';
 import Pizza from '../Components/Menu Components/Pizza/Pizza';
 import Salad from '../Components/Menu Components/Salads/Salad';
+import Soup from '../Components/Menu Components/Soups/Soup';
 
 const Menu = () => {
     return (
@@ -13,6 +14,7 @@ const Menu = () => {
             <Deserts></Deserts>
             <Pizza></Pizza>
             <Salad></Salad>
+            <Soup></Soup>
         </div>
     );
 };
