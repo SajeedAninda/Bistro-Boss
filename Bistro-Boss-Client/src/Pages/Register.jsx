28387011ba26875eb1ba2ab4fa67c1d5718ba78c
SajeedAninda <sajaninda@gmail.com>
2023-11-13 16:22:37 +0600

    Created Register Page

--- Bistro-Boss-Client/src/Pages/Register.jsx
+++ Bistro-Boss-Client/src/Pages/Register.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import backgroundImage from "../../src/assets/others/authentication.png"
+import authImg from "../../src/assets/others/authentication2.png"
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const divStyle = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+    };
+    const shadowedDiv = {
+        backgroundImage: `url(${backgroundImage})`,
+        boxShadow: '10px 10px 10px 10px rgba(0, 0, 0, 0.25)'
+    };
+
+
     return (
-        <div>
-            
+        <div style={divStyle} className='min-h-screen flex justify-center items-center py-20'>
+            <div style={shadowedDiv} className='w-[90%] px-20 py-12 flex justify-center items-center' >
+                <div className='flex-1'>
+                    <div className='text-center'>
+                        <h1 className='text-[#151515] text-3xl font-bold'>Sign Up </h1>
+                    </div>
+                    <form>
+                        <div>
+                            <label className='text-[#444] font-semibold' htmlFor="name">Name</label> <br />
+                            <input className='py-3 px-4 placeholder:text-sm mt-2 w-full rounded-md' type="text" name='name' placeholder='Type your name' required />
+                        </div>
+
+                        <div className='mt-5'>
+                            <label className='text-[#444] font-semibold' htmlFor="name">Email</label> <br />
+                            <input className='py-3 px-4 placeholder:text-sm mt-2 w-full rounded-md' type="email" name='email' placeholder='Type your email' required />
+                        </div>
+
+                        <div className='mt-5'>
+                            <label className='text-[#444] font-semibold' htmlFor="name">Photo URL</label> <br />
+                            <input className='py-3 px-4 placeholder:text-sm mt-2 w-full rounded-md' type="text" name='photoUrl' placeholder='Enter a Photo Url' required />
+                        </div>
+
+                        <div className='mt-5'>
+                            <label className='text-[#444] font-semibold' htmlFor="name">Password</label> <br />
+                            <input className='py-3 px-4 placeholder:text-sm mt-2 w-full rounded-md' type="password" name='password' placeholder='Enter Your Password' required />
+                        </div>
+
+                        <button className='w-full py-3 text-white border-2 border-[#D1A054] font-bold bg-[#D1A054] mt-5 rounded-md hover:bg-transparent hover:border-2 hover:border-[#D1A054] hover:text-[#D1A054]' type="submit">Sign Up</button>
+                        <p className='text-[#D1A054] font-medium text-center mt-3'>Already registered? Go to <span><Link className='font-bold hover:underline' to={"/login"}>Login</Link></span></p>
+                    </form>
+                </div>
+
+                <div className='flex-1'>
+                    <img src={authImg} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
