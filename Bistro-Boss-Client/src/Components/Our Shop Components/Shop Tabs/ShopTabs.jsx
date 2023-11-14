654350246f459c6da04e66740b9fd6cc624bf57c
SajeedAninda <sajaninda@gmail.com>
2023-11-14 18:51:32 +0600

    Implemented Add product to cart and Check for error if product exists(User Specific)

--- Bistro-Boss-Client/src/Components/Our Shop Components/Shop Tabs/ShopTabs.jsx
+++ Bistro-Boss-Client/src/Components/Our Shop Components/Shop Tabs/ShopTabs.jsx
@@ -8,11 +8,11 @@ import TabCard from './TabCard';
 const ShopTabs = () => {
     const [tabIndex, setTabIndex] = useState(0);
     let menu = UseMenu();
-    let salad = menu.filter(item => item.category === "salad");
-    let pizza = menu.filter(item => item.category === "pizza");
-    let soup = menu.filter(item => item.category === "soup");
-    let deserts = menu.filter(item => item.category === "dessert");
-    let drinks = menu.filter(item => item.category === "drinks");
+    let salad = menu?.filter(item => item.category === "salad");
+    let pizza = menu?.filter(item => item.category === "pizza");
+    let soup = menu?.filter(item => item.category === "soup");
+    let deserts = menu?.filter(item => item.category === "dessert");
+    let drinks = menu?.filter(item => item.category === "drinks");
 
     return (
         <div className='text-center text-[#BB8506] mt-12 w-[70%] mx-auto'>
@@ -27,29 +27,21 @@ const ShopTabs = () => {
                 <TabPanel>
                     <div className='grid grid-cols-3 gap-6 py-12 mb-20'>
                         {
-                            salad.map(item => <TabCard tabItem={item}></TabCard>)
+                            salad?.map(item => <TabCard tabItem={item}></TabCard>)
                         }
                     </div>
                 </TabPanel>
                 <TabPanel>
                     <div className='grid grid-cols-3 gap-6 py-12 mb-20'>
                         {
-                            pizza.map(item => <TabCard tabItem={item}></TabCard>)
+                            pizza?.map(item => <TabCard tabItem={item}></TabCard>)
                         }
                     </div>
                 </TabPanel>
                 <TabPanel>
                     <div className='grid grid-cols-3 gap-6 py-12 mb-20'>
                         {
-                            soup.map(item => <TabCard tabItem={item}></TabCard>)
-                        }
-                    </div>
-                </TabPanel>
-
-                <TabPanel>
-                    <div className='grid grid-cols-3 gap-6 py-12 mb-20'>
-                        {
-                            deserts.map(item => <TabCard tabItem={item}></TabCard>)
+                            soup?.map(item => <TabCard tabItem={item}></TabCard>)
                         }
                     </div>
                 </TabPanel>
@@ -57,7 +49,15 @@ const ShopTabs = () => {
                 <TabPanel>
                     <div className='grid grid-cols-3 gap-6 py-12 mb-20'>
                         {
-                            drinks.map(item => <TabCard tabItem={item}></TabCard>)
+                            deserts?.map(item => <TabCard tabItem={item}></TabCard>)
+                        }
+                    </div>
+                </TabPanel>
+
+                <TabPanel>
+                    <div className='grid grid-cols-3 gap-6 py-12 mb-20'>
+                        {
+                            drinks?.map(item => <TabCard tabItem={item}></TabCard>)
                         }
                     </div>
                 </TabPanel>
--- Bistro-Boss-Client/src/Components/Our Shop Components/Shop Tabs/TabCard.jsx
+++ Bistro-Boss-Client/src/Components/Our Shop Components/Shop Tabs/TabCard.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
+import UseAuth from '../../../Hooks/UseAuth';
+import useAxiosInstance from '../../../Hooks/UseAxiosInstance';
+import toast from 'react-hot-toast';
 
 const TabCard = ({ tabItem }) => {
+    let axiosInstance = useAxiosInstance();
+    let { loggedInUser } = UseAuth();
+    let currentUserEmail = loggedInUser?.email;
+    let prevId = tabItem._id;
+    let { name, recipe, image, category, price } = tabItem;
+
+    let handleAddToCart = async () => {
+        let cartItem = { name, recipe, image, category, price, prevId, currentUserEmail }
+        try {
+            const response = await axiosInstance.post("/cart", cartItem, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.data.insertedId) {
+                toast.success(`${name} added to Cart`)
+            };
+        } catch (error) {
+            if (error.response && error.response.status === 400) {
+                toast.error("Product already exists in the cart");
+            } else {
+                console.error(error);
+                toast.error("Error adding product to cart");
+            }
+        }
+    }
+
+
     return (
         <div className='mt-6 flex flex-col'>
             <img className='h-[200px] object-cover' src={tabItem.image} alt="" />
             <div className='bg-[#F3F3F3] flex justify-between items-center flex-col text-center py-8 h-[250px]'>
                 <h2 className='text-lg text-[#151515] font-semibold'>{tabItem.name}</h2>
                 <p className='text-[#151515] mt-2'>{tabItem.recipe}</p>
-                <button className='px-4 py-3 mt-3 text-[#BB8506] bg-[#E8E8E8] rounded-md border-b-2 border-[#BB8506] hover:bg-[#1F2937] hover:border-[#1F2937]'>Add to Cart</button>
+                <button onClick={handleAddToCart} className='px-4 py-3 mt-3 text-[#BB8506] bg-[#E8E8E8] rounded-md border-b-2 border-[#BB8506] hover:bg-[#1F2937] hover:border-[#1F2937]'>Add to Cart</button>
             </div>
         </div>
     );
