2edf4c166f265926b7af968cad7f14b42ad58c32
SajeedAninda <sajaninda@gmail.com>
2023-11-14 20:05:14 +0600

    Implemented Deleted from Cart Functionalities

--- Bistro-Boss-Client/src/Components/User Home/User Cart/UserCart.jsx
+++ Bistro-Boss-Client/src/Components/User Home/User Cart/UserCart.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import SectionHeader from '../../Shared/Section Header/SectionHeader';
 import { FaRegTrashAlt } from "react-icons/fa";
 import useCartData from '../../../Hooks/useCartData';
-
+import useAxiosInstance from '../../../Hooks/UseAxiosInstance';
+import toast from 'react-hot-toast';
 
 const UserCart = () => {
     const headerStyle = {
@@ -10,10 +11,23 @@ const UserCart = () => {
     };
     let [cartData, refetch] = useCartData();
     let totalPrice = cartData?.reduce((acc, item) => acc + item.price, 0);
+    let axiosInstance = useAxiosInstance();
+
+    let handleDeleteFromCart = async (id) => {
+        try {
+            let response = await axiosInstance.delete(`/cart/${id}`);
+            if (response.data.deletedCount > 0) {
+                refetch();
+                toast.success("Deleted From Cart Succesfully");
+            }
+        } catch (error) {
+            console.error('Error deleting item:', error.message);
+        }
+    };
 
     return (
         <div className='w-[80%] bg-[#F6F6F6]'>
-            <SectionHeader primaryText={"WANNA ADD MORE?"} seconddaryText={"---My Cart---"}></SectionHeader>
+            <SectionHeader primaryText={"WANNA ADD MORE?"} seconddaryText={"---My Cart---"} />
             <div className='bg-white px-8 py-8 w-[80%] mx-auto'>
                 <div className='flex justify-between items-center'>
                     <h2 style={headerStyle} className='text-2xl text-[#151515] font-bold'>Total orders: {cartData?.length}</h2>
@@ -30,14 +44,14 @@ const UserCart = () => {
                 </div>
 
                 {cartData?.map((cartItem, index) =>
-                    <div className='px-10 py-4 rounded-t-2xl  grid grid-cols-9 items-center border-b border-[]'>
+                    <div key={cartItem._id} className='px-10 py-4 rounded-t-2xl  grid grid-cols-9 items-center border-b border-[]'>
                         <h2 className='text-black font-semibold col-span-1'>{index + 1}</h2>
                         <div className='text-[#737373] font-semibold col-span-2 w-[65px] h-[65px]'>
                             <img src={cartItem?.image} alt="" />
                         </div>
                         <h3 className='text-[#737373] font-semibold col-span-4'>{cartItem?.name}</h3>
                         <h3 className='text-[#737373] font-semibold col-span-1'>$ {cartItem?.price}</h3>
-                        <button className='col-span-1 flex justify-center w-[50%] mx-auto  py-3 text-white bg-[#B91C1C] rounded-lg'>
+                        <button onClick={() => handleDeleteFromCart(cartItem?._id)} className='col-span-1 flex justify-center w-[50%] mx-auto  py-3 text-white bg-[#B91C1C] rounded-lg'>
                             <FaRegTrashAlt />
                         </button>
                     </div>
